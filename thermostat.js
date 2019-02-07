d6d7937600af2f294828c471e9f9b14166093fda
ellie turnock <ebturnock@gmail.com>
2019-02-07 16:17:50 +0000

    Default temperature added to Thermostat

---
+++ thermostat.js
@@ -0,0 +1,10 @@
+'use strict';
+//create class
+class Thermostat {
+  //create constructor to initialize functions
+  constructor(temperature = 20) {
+    this.temperature = temperature;
+  }
+};
+
+module.exports = Thermostat;
