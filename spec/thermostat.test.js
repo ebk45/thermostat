590333ef7b0a08dbd2a006e3bf98c3dbfef321be
ellie turnock <ebturnock@gmail.com>
2019-02-08 16:07:44 +0000

    Test: Minimum temperature for down function, refactored tests and added describe block

--- spec/thermostat.test.js
+++ spec/thermostat.test.js
@@ -2,22 +2,32 @@
 
 const Thermostat = require('../thermostat.js');
 
-var thermostat = null;
+describe('Thermostat', () => {
 
-beforeEach(() => {
-  thermostat = new Thermostat();
-});
+  let thermostat;
 
-test('default temperature set to 20 degrees', () => {
-  expect(thermostat.temperature).toEqual(20);
-});
+  beforeEach(() => {
+    thermostat = new Thermostat();
+  });
 
-test('temperature can be increased with "up"', () => {
-  thermostat.up()
-  expect(thermostat.temperature).toEqual(21);
-});
+  test('default temperature set to 20 degrees', () => {
+    expect(thermostat.temperature).toEqual(20);
+  });
 
-test('temperature can be decreased with "down"', () => {
-  thermostat.down()
-  expect(thermostat.temperature).toEqual(19);
+  test('temperature can be increased with "up"', () => {
+    thermostat.up()
+    expect(thermostat.temperature).toEqual(21);
+  });
+
+  test('temperature can be decreased with "down"', () => {
+    thermostat.down()
+    expect(thermostat.temperature).toEqual(19);
+  });
+
+  test('temperature does not go below 10 degrees', () => {
+    for (let i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.temperature).toEqual(10);
+  });
 });
