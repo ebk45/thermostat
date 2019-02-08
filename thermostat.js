47b58eabaf3787a0ee98a2483236177d14f61815
ellie turnock <ebturnock@gmail.com>
2019-02-08 16:13:31 +0000

    Down function now has minimum temperature

--- thermostat.js
+++ thermostat.js
@@ -4,15 +4,20 @@ class Thermostat {
   //create constructor to initialize functions
   constructor(temperature = 20) {
     this.temperature = temperature;
-  }
+    this._minTemp = 10;
+  };
 
   up() {
     this.temperature += 1
-  }
+  };
 
   down() {
-    this.temperature -= 1
-  }
+    if (this.temperature === this._minTemp) {
+      this.temperature = this._minTemp
+    } else {
+      this.temperature -= 1
+    }
+  };
 };
 
 module.exports = Thermostat;
